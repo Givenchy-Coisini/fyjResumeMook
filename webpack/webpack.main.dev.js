08627c2e7f48bbb8e3a9b94528bbb4a7fa86cf8d
Givenchy-Coisini <2312696405@qq.com>
2021-07-14 19:37:20 +0800

    feat:完成Electron与React结合

---
+++ webpack/webpack.main.dev.js
@@ -0,0 +1,21 @@
+/**
+ * 主进程配置
+ */
+
+ const path = require('path');
+ const webpack = require('webpack');
+ const baseConfig = require('./webpack.base.js');
+ const webpackMerge = require('webpack-merge');
+ 
+ const mainConfig = {
+   entry: path.resolve(__dirname, '../app/main/electron.js'),
+   target: 'electron-main',
+   output: {
+     filename: 'electron.js',
+     path: path.resolve(__dirname, '../dist'),
+   },
+   devtool: 'inline-source-map',
+   mode: 'development',
+ };
+ 
+ module.exports = webpackMerge.merge(baseConfig, mainConfig);
