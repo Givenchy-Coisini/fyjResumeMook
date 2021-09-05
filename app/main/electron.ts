12c75339c12de43231eb6c2ae14540065a68b7ef
Givenchy-Coisini <2312696405@qq.com>
2021-09-05 23:51:04 +0800

    feat:代码优化

--- app/main/electron.ts
+++ app/main/electron.ts
@@ -4,12 +4,6 @@
 import path from 'path';
 import { app, BrowserWindow, ipcMain } from 'electron';
 
-const ROOT_PATH = path.join(app.getAppPath(), '../');
-
-ipcMain.on('get-root-path', (event, arg) => {
-  event.reply('reply-root-path', ROOT_PATH);
-});
-
 function isDev() {
   // 👉 还记得我们配置中通过 webpack.DefinePlugin 定义的构建变量吗
   return process.env.NODE_ENV === 'development';
@@ -39,4 +33,10 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+});
+
+const ROOT_PATH = path.join(app.getAppPath(), '../');
+
+ipcMain.on('get-root-path', (event, arg) => {
+  event.reply('reply-root-path', ROOT_PATH);
 });
--- app/renderer/common/utils/appPath.ts
+++ app/renderer/common/utils/appPath.ts
@@ -1,10 +1,8 @@
-// renderer/common/utils/appPath.ts
-
 // 监听主进程与渲染进程通信
 import { ipcRenderer } from 'electron';
 
 // 获取项目绝对路径
-export function getAppPath() {
+export function getAppPath(): Promise<string> {
   return new Promise((resolve: (value: string) => void, reject: (value: Error) => void) => {
     ipcRenderer.send('get-root-path', '');
     ipcRenderer.on('reply-root-path', (event, arg: string) => {
