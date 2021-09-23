4106185cd39fcb19a6aba54f6940e374779db9d0
Givenchy-Coisini <2312696405@qq.com>
2021-09-23 15:47:52 +0800

    feat:模板页面头部组件

---
+++ app/renderer/container/templateList/index.tsx
@@ -0,0 +1,13 @@
+// renderer/container/templateList/index.tsx
+import React from 'react';
+import './index.less';
+import Header from './Header';
+
+function TemplateList() {
+  return (
+    <div styleName="container">
+      <Header />
+    </div>
+  );
+}
+export default TemplateList;
