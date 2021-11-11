e972e2c92770c8b101449709341f9be5d94ff449
Givenchy-Coisini <2312696405@qq.com>
2021-11-11 16:51:27 +0800

    feat:解决路由缓存问题和高度问题

--- app/renderer/router.tsx
+++ app/renderer/router.tsx
@@ -1,11 +1,19 @@
+/*
+ * @Description:
+ * @Author: pengdaokuan
+ * @LastEditors: pengdaokuan
+ * @Date: 2021-06-25 08:56:12
+ * @LastEditTime: 2021-07-26 09:24:20
+ */
 import React, { useEffect } from 'react';
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import CacheRoute, { CacheSwitch } from 'react-router-cache-route';
+import { HashRouter, Redirect } from 'react-router-dom';
 import Root from '@src/container/root';
 import Resume from '@src/container/resume';
 import TemplateList from '@src/container/templateList';
-import useReadDirAssetsTemplateHooks from './hooks/useReadDirAssetsTemplateHooks';
-import useThemeActionHooks from './hooks/useThemeActionHooks';
 import ROUTER from '@common/constants/router';
+import useThemeActionHooks from './hooks/useThemeActionHooks';
+import useReadDirAssetsTemplateHooks from './hooks/useReadDirAssetsTemplateHooks';
 
 function Router() {
   const readDirAssetsTemplateHooks = useReadDirAssetsTemplateHooks();
@@ -14,22 +22,15 @@ function Router() {
     initThemeConfig();
     readDirAssetsTemplateHooks();
   }, []);
+
   return (
     <HashRouter>
-      <Switch>
-        {/* 一定要添加 exact */}
-        <Route path={ROUTER.root} exact>
-          <Root />
-        </Route>
-        <Route path={ROUTER.resume} exact>
-          <Resume />
-        </Route>
-        <Route path={ROUTER.templateList} exact>
-          <TemplateList />
-        </Route>
-      </Switch>
-      {/* 默认重定向到首页 */}
-      <Redirect to={ROUTER.root} />
+      <CacheSwitch>
+        <CacheRoute path={ROUTER.root} exact component={Root} />
+        <CacheRoute path={ROUTER.resume} exact component={Resume} />
+        <CacheRoute path={ROUTER.templateList} exact component={TemplateList} />
+        <Redirect from={ROUTER.root} exact to={ROUTER.root} />
+      </CacheSwitch>
     </HashRouter>
   );
 }
